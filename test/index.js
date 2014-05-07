28e6231c8011c4052d1e3c93bc2d4f2704c71597
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-07 14:27:30 +0200

    better error handling, now same as Connect

--- test/index.js
+++ test/index.js
@@ -132,32 +132,85 @@ describe('app, middleware manager', function () {
         expect(res.count).to.equal(2);
       });
     });
+
+    it('should call only error middlewares when an error occured', function() {
+      app
+      .use('test', exceptionMiddleware)
+      .use('test', errorHandlingMiddleware)
+      .use('test', transformMiddleware)
+      .handle('test', req, res, function(err, req, res) {
+        expect(err).to.equal('error handled');
+        expect(res.test).to.not.exists;
+      });
+    });
+
+    it('should not call error middlewares when no error', function () {
+      app
+      .use('test', transformMiddleware)
+      .use('test', errorHandlingMiddleware)
+      .handle('test', req, res, function(err, req, res) {
+        expect(err).to.not.exists;
+      });
+    });
+
+    it('should call all error middlewares error once an error occured', function () {
+      app
+      .use('test', exceptionMiddleware)
+      .use('test', errorHandlingMiddleware)
+      .use('test', anotherErrorHandlingMiddleware)
+      .handle('test', req, res, function(err, req, res) {
+        expect(err).to.equal('error also handled');
+      });
+    });
+
+    it('should resume normal middleware execution if error handler ignores error', function () {
+      app
+      .use('test', exceptionMiddleware)
+      .use('test', ignoreErrorHandler)
+      .use('test', transformMiddleware)
+      .handle('test', req, res, function(err, req, res) {
+        expect(err).to.not.exists;
+        expect(res.test).to.equal('test');
+      });
+    });
   });
 });
 
-function testMiddleWare(err, req, res, next) {
+function testMiddleWare(req, res, next) {
   next();
 }
 
-function endMiddleware(err, req, res, next) {
+function endMiddleware(req, res, next) {
   res.end();
 }
 
-function incMiddleware(err, req, res, next) {
+function incMiddleware(req, res, next) {
   if (!res.count) res.count = 0;
   res.count++;
   next();
 }
 
-function transformMiddleware(err, req, res, next) {
+function transformMiddleware(req, res, next) {
   res.test = 'test';
   next();
 }
 
-function exceptionMiddleware(err, req, res, next) {
+function exceptionMiddleware(req, res, next) {
   throw 'oups !';
 }
 
-function errorMiddleWare(err, req, res, next) {
+function errorMiddleWare(req, res, next) {
   next('oh no !');
+}
+
+function errorHandlingMiddleware(err, req, res, next) {
+  next('error handled');
+}
+
+function anotherErrorHandlingMiddleware(err, req, res, next) {
+  next('error also handled');
+}
+
+function ignoreErrorHandler(err, req, res, next) {
+  next();
 }
